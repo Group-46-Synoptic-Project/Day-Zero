085c440b167ed24371db8a6595f3b34268b3b7ef
arif-codes <arifahmed687@gmail.com>
2023-06-14 12:04:45 +0100

    added png for govan stones and video embeded

--- javascript/features/videos.js
+++ javascript/features/videos.js
@@ -3,4 +3,5 @@ const videos ={
     riverside_museum_video: "https://www.youtube.com/embed/7snOXhaR6sw",
     govan_old_video: "https://www.youtube.com/embed/NabsHvns1C8",
     glenlee_ship_video: "https://www.youtube.com/embed/VeHyrD3XR10",
+    govan_stones_video: "https://www.youtube.com/embed/gh6W3Yop-18",
 }
