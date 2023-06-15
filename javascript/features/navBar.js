7a116d4553addcdb885f5b2225ebd23fe9a4fb77
Seb-Taylor <Seb.Taylor@uea.ac.uk>
2023-06-15 09:05:59 +0100

    fixed menu names, added head comment

--- javascript/features/navBar.js
+++ javascript/features/navBar.js
@@ -1,8 +1,22 @@
+/*
+ File: navBar.js
+ Description: This JavaScript file handles navigation for the website, 
+  including the functionality of a hamburger menu.
+ 
+ Functions:
+  1. Hamburger menu toggle - This opens and closes the hamburger menu on user interaction.
+  2. Redirection to 'languages.html' - This function handles redirection to the 'languages' page when the 'languagesSymbol' element is clicked.
+ 
+ Version History:
+  - Version 1.0 (June 13, 2023): Initial release, complete fucntionality.
+ 
+ Author: Seb Taylor
+ */
 
 // HAMBURGER MENU================================================
-var menuToggle = document.getElementById('menu-toggle');
+var menuToggle = document.getElementById('menuToggle');
 var menu = document.getElementById('menu');
-var dropDown = document.getElementById('dropdownIcon');
+var menuIcon = document.getElementById('menuIcon');
 var closeIcon = document.getElementById('closeIcon');
 
 //show menu
@@ -17,10 +31,9 @@ closeIcon.addEventListener('click', function() {
 
 //hide menu when clicking outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('#dropdownIcon')) {
+  if (!event.target.matches('#menuIcon')) {
     if (menu.classList.contains('show')) { //if menu is showing
       menu.classList.remove('show'); //hide menu
-      dropDown.classList.remove('cross');
     }
   }
 }
